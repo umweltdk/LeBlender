3b50cb8a7331340755e628b118442734d14229a5
Lars Landbo olsen <lars.landbo.olsen@gmail.com>
2020-04-17 14:42:24 +0200

    Add public helper to get gridConfigs without http requests

---
+++ Src/Lecoati.LeBlender.Extension/Helpers/GridEditorHelper.cs
@@ -0,0 +1,23 @@
+﻿using Lecoati.LeBlender.Extension.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lecoati.LeBlender.Extension.Helpers
+{
+    public class GridEditorHelper
+    {
+        public static IOrderedEnumerable<LeBlenderGridEditorModel> GetEditors()
+        {
+            var editors = RuntimeCacheHelper.GetCachedItem<IOrderedEnumerable<LeBlenderGridEditorModel>>("LeBlenderEditors");
+            if (editors == null)
+            {
+                var dbHelper = new DatabaseHelper();
+                editors = dbHelper.GetEditors();
+                RuntimeCacheHelper.SetCacheItem("LeBlenderEditors", editors, 1);
+            }
+            return editors;
+        }
+    }
+}
